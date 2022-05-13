a16f29098732eb2e19dae4b75435409208eb63c5
SnChul <104672461+SnChul@users.noreply.github.com>
2022-05-13 23:16:16 +0900

    [UPDATE] 세이브 버튼 기능추가 파일 생성 및 로딩기능 추가예정

--- Click.py
+++ Click.py
@@ -1,27 +1,43 @@
 
 from tkinter import *
-
+from tkinter import filedialog
 
 root=Tk()
 root.title("Count Click")
 root.geometry("300x200+1100+500")
 root.resizable(False, False)
-
 Clk = 0        
+# file = open('C:/Users/we202/Documents/GitHub/DevApp/count.txt', 'w')
+# Clk2 = file
+# file.close
+# print(Clk2)
+Click=Label(root, text="Clicked : ").place(x=100, y=70)
+Click2=Label(root, text="Total : ").place(x=100, y=90)
 
+#클릭시 카운트 올라감
 def clickevent(self):
     global Clk 
+    # global Clk2
     Clk += 1
+    # Clk2 += 1
     global Click
     Click=Label(root, text="Clicked : "+str(Clk)).place(x=100, y=70)
     Click2=Label(root, text="Total : "+str(Clk)).place(x=100, y=90)   
-def Save(self):
-    print("saved")    
+    
+#세이브파일 생성 기능 추가 예정    
+def Save():
+    filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
+    filetypes=(("TXT files", "*.txt"),("all files", "*.*")))
+    
+
+
+
+#위젯    
 Today = Label(root, text="TODAY").place(x=130, y=30)
 root.bind("<Button-1>", clickevent)
-Click=Label(root, text="Clicked : ").place(x=100, y=70)
-Click2=Label(root, text="Total : ").place(x=100, y=90)
+
 Save = Button(root, text="save", command=Save).place(x=10,y=10) 
+
 
 
 root.mainloop()
